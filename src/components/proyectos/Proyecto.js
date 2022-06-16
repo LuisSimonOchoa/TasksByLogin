57de2ead52ddc171b91848fdaa756cd0568c82f2
Luis Simon <lsimonochoa@gmail.com>
2022-06-15 23:14:31 -0500

    update 2.0

--- src/components/proyectos/Proyecto.js
+++ src/components/proyectos/Proyecto.js
@@ -1,5 +1,6 @@
 import React, {useContext } from 'react';
 import proyectoContext from '../../context/proyectos/proyectoContext';
+import TareaContext from '../../context/tareas/tareaContext';
 
 const Proyecto = ({proyecto}) => {
 
@@ -7,12 +8,22 @@ const Proyecto = ({proyecto}) => {
     const proyectosContext = useContext(proyectoContext);
     const { proyectoActual } = proyectosContext;
 
+    // Obtener la función del context de tarea
+    const tareasContext = useContext(TareaContext);
+    const { obtenerTareas } = tareasContext;
+
+    // Función para agregar el proyecto actual
+    const seleccionarProyecto = id => {
+        proyectoActual(id); // Fijar un proyecto actual
+        obtenerTareas(id);
+    }
+
     return (
         <li>
             <button
                 type='button'
                 className='btn btn-blank'
-                onClick={() => proyectoActual(proyecto.id)}
+                onClick={() => seleccionarProyecto(proyecto.id)}
             >{proyecto.nombre}</button>
         </li>
     );
