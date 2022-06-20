e9dca114d463d3f0c6aeacd7565b2fd51669c5ee
Luis Simon <lsimonochoa@gmail.com>
2022-06-19 21:57:51 -0500

    update 2.1

--- src/components/tareas/Tarea.js
+++ src/components/tareas/Tarea.js
@@ -1,6 +1,41 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import TareaContext from '../../context/tareas/tareaContext';
+import proyectoContext from '../../context/proyectos/proyectoContext';
 
 const Tarea = ({tarea}) => {
+
+    // Extraer is un proyecto está activo
+    const proyectosContext = useContext(proyectoContext);
+    const { proyecto } = proyectosContext;
+
+    // Obtener la función del context de tarea
+    const tareasContext = useContext(TareaContext);
+    const { eliminarTarea, obtenerTareas, cambiarEstadoTarea, guardarTareaActual } = tareasContext;
+
+    // Extraer el proyecto
+    const [ proyectoActual ] = proyecto;
+
+    // Función que se ejecuta cuando el ususario presiona el btn de eliminar tarea
+    const tareaEliminar = id => {
+        eliminarTarea(id);
+        obtenerTareas(proyectoActual.id)
+    }
+
+    // Función que modifica el estado de las tareas
+    const cambiarEstado = tarea => {
+        if(tarea.estado) {
+            tarea.estado = false;
+        } else {
+            tarea.estado = true;
+        }
+        cambiarEstadoTarea(tarea);
+    }
+
+    // Agrega una tarea actual cuando el usuario desea editarla
+    const seleccionarTarea = tarea => {
+        guardarTareaActual(tarea);
+    }
+
     return (
         <li className='tarea sombra'>
             <p>{tarea.nombre}</p>
@@ -10,12 +45,14 @@ const Tarea = ({tarea}) => {
                         <button
                             type='button'
                             className='completo'
+                            onClick={() => cambiarEstado(tarea)}
                         >Completo</button>
                     )
                     : (
                         <button
                             type='button'
                             className='incompleto'
+                            onClick={() =>cambiarEstado(tarea)}
                         >Incompleto</button>
                     )
                 }
@@ -24,10 +61,12 @@ const Tarea = ({tarea}) => {
                 <button
                     type='button'
                     className='btn btn-primario'
+                    onClick={() => seleccionarTarea(tarea)}
                 >Editar</button>
                 <button
                     type='button'
                     className='btn btn-secundario'
+                    onClick={() => tareaEliminar(tarea.id)}
                 >Eliminar</button>
             </div>
         </li>
--- src/context/tareas/tareaState.js
+++ src/context/tareas/tareaState.js
@@ -1,28 +1,37 @@
 import React, { useReducer } from 'react';
 import TareaContext from './tareaContext';
 import tareaReducer from './tareaReducer';
+import { v4 as uuidv4 } from 'uuid';
 import { 
-    TAREAS_PROYECTO 
+    TAREAS_PROYECTO,
+    AGREGAR_TAREA,
+    VALIDAR_TAREA,
+    ELIMINAR_TAREA,
+    ESTADO_TAREA,
+    TAREA_ACTUAL,
+    ACTUALIZAR_TAREA
 } from '../../types';
 
 const TareaState = props => {
     const initialState = {
         tareas: [
-            {nombre: 'Elegir Plataforma', estado: true, proyectoId: 1},
-            {nombre: 'Elegir Colores', estado: false, proyectoId: 2},
-            {nombre: 'Elegir Plataforma de pago', estado: false, proyectoId: 3},
-            {nombre: 'Elegir Hosting', estado: true, proyectoId: 4},
-            {nombre: 'Elegir Plataforma', estado: true, proyectoId: 1},
-            {nombre: 'Elegir Colores', estado: false, proyectoId: 2},
-            {nombre: 'Elegir Plataforma de pago', estado: false, proyectoId: 3},
-            {nombre: 'Elegir Hosting', estado: true, proyectoId: 4},
-            {nombre: 'Elegir Plataforma', estado: true, proyectoId: 1},
-            {nombre: 'Elegir Colores', estado: false, proyectoId: 2},
-            {nombre: 'Elegir Plataforma de pago', estado: false, proyectoId: 3},
-            {nombre: 'Elegir Hosting', estado: true, proyectoId: 4},
-            {nombre: 'Elegir Plataforma', estado: true, proyectoId: 3},
+            {id: 1, nombre: 'Elegir Plataforma', estado: true, proyectoId: 1},
+            {id: 2, nombre: 'Elegir Colores', estado: false, proyectoId: 2},
+            {id: 3, nombre: 'Elegir Plataforma de pago', estado: false, proyectoId: 3},
+            {id: 4, nombre: 'Elegir Hosting', estado: true, proyectoId: 4},
+            {id: 5, nombre: 'Elegir Plataforma', estado: true, proyectoId: 1},
+            {id: 6, nombre: 'Elegir Colores', estado: false, proyectoId: 2},
+            {id: 7, nombre: 'Elegir Plataforma de pago', estado: false, proyectoId: 3},
+            {id: 8, nombre: 'Elegir Hosting', estado: true, proyectoId: 4},
+            {id: 9, nombre: 'Elegir Plataforma', estado: true, proyectoId: 1},
+            {id: 10, nombre: 'Elegir Colores', estado: false, proyectoId: 2},
+            {id: 11, nombre: 'Elegir Plataforma de pago', estado: false, proyectoId: 3},
+            {id: 12, nombre: 'Elegir Hosting', estado: true, proyectoId: 4},
+            {id: 13, nombre: 'Elegir Plataforma', estado: true, proyectoId: 3},
         ],
-        tareasproyecto: null
+        tareasproyecto: null,
+        errortarea: false,
+        tareaseleccionada: null
     }
 
     // Crear dispatch y state
@@ -36,12 +45,68 @@ const TareaState = props => {
         })
     }
 
+    // Agregar una tarea al proyecto seleccionado
+    const agregarTarea = tarea => {
+        tarea.id = uuidv4();
+        Dispatch({
+            type: AGREGAR_TAREA,
+            payload: tarea
+        })
+    }
+
+    // Valida y muestra un error en caso de que sea necesario
+    const validarTarea = () => {
+        Dispatch({
+            type: VALIDAR_TAREA
+        })
+    }
+
+    // Eliminar tarea por id
+    const eliminarTarea = id => {
+        Dispatch({
+            type: ELIMINAR_TAREA,
+            payload: id
+        })
+    }
+
+    // Cambia el estado de cada tarea
+    const cambiarEstadoTarea = tarea => {
+        Dispatch({
+            type: ESTADO_TAREA,
+            payload: tarea
+        })
+    }
+
+    // Extrae una tarea para edición
+    const guardarTareaActual = tarea => {
+        Dispatch({
+            type: TAREA_ACTUAL,
+            payload: tarea
+        })
+    }
+
+    // Edita o modifica una tarea
+    const actualizarTarea = tarea => {
+        Dispatch({
+            type: ACTUALIZAR_TAREA,
+            payload: tarea
+        })
+    }
+
     return (
         <TareaContext.Provider
             value={{
                 tareas: state.tareas,
                 tareasproyecto: state.tareasproyecto,
-                obtenerTareas
+                errortarea: state.errortarea,
+                tareaseleccionada: state.tareaseleccionada,
+                obtenerTareas,
+                agregarTarea,
+                validarTarea,
+                eliminarTarea,
+                cambiarEstadoTarea,
+                guardarTareaActual,
+                actualizarTarea
             }}
         >
             {props.children}
